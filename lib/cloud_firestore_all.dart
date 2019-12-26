762f3066bd2cf15b4ca9881e5844f1245e906caa
LucazzP <lucashpolazzo2013@gmail.com>
2019-12-26 11:16:42 -0300

    Added timestamp

--- lib/cloud_firestore_all.dart
+++ lib/cloud_firestore_all.dart
@@ -1,5 +1,8 @@
 library cloud_firestore_all;
+import 'dart:ui' show hashValues;
 
 export 'src/interface.dart'
     if (dart.library.io) 'src/mobile.dart'
     if (dart.library.js) 'src/web.dart';
+
+part 'src/timestamp.dart';
